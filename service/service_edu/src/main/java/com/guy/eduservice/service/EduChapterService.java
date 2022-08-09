373af078a675b9f63f49bd08f1d2df7542bdd339
GuY <hanyingbamboo@outlook.com>
2022-08-09 23:29:12 +0800

    2022.8.9

--- service/service_edu/src/main/java/com/guy/eduservice/service/EduChapterService.java
+++ service/service_edu/src/main/java/com/guy/eduservice/service/EduChapterService.java
@@ -2,6 +2,9 @@ package com.guy.eduservice.service;
 
 import com.guy.eduservice.entity.EduChapter;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.guy.eduservice.entity.chapter.ChapterVo;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +16,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface EduChapterService extends IService<EduChapter> {
 
+    //课程大纲列表，根据课程id进行查询
+    List<ChapterVo> getChapterVideoByCourseId(String courseId);
 }
